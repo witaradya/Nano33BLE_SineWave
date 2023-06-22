ba37cb60fbb651588c4fdc80d5fc0e603094a157
witaradya <witaradya.a.dharma@polytron.co.id>
2023-06-22 15:48:54 +0700

    Add all program

---
+++ Nano33BLE_SineWave.ino
@@ -0,0 +1,123 @@
+// Import TensorFlow stuff
+#include <TensorFlowLite.h>
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_log.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+// Our model
+#include "sine_model.h"
+
+// Figure out what's going on in our model
+#define DEBUG 1
+
+// Some settings
+constexpr int led_pin = 2;
+constexpr float pi = 3.14159265;                  // Some pi
+constexpr float freq = 0.09;                       // Frequency (Hz) of sinewave
+constexpr float period = (1 / freq) * (1000);  // Period (miliseconds)
+
+// TFLite globals, used for compatibility with Arduino-style sketches
+namespace {
+  const tflite::Model* model = nullptr;
+  tflite::MicroInterpreter* interpreter = nullptr; 
+  TfLiteTensor* model_input = nullptr;
+  TfLiteTensor* model_output = nullptr;
+  constexpr int kTensorArenaSize = 5 * 1024;
+  alignas(16) uint8_t tensor_arena[kTensorArenaSize];
+}
+
+
+void setup() {
+#if DEBUG
+  while (!Serial);
+#endif
+
+  pinMode(led_pin, OUTPUT);
+
+  tflite::InitializeTarget();
+
+  // Map the model into a usable data structure
+  model = tflite::GetModel(sine_model);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    MicroPrintf(
+      "Model provided is schema version %d not equal "
+      "to supported version %d.",
+      model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+
+  // This pulls in all the operation implementations we need.
+  // NOLINTNEXTLINE(runtime-global-variables)
+  static tflite::AllOpsResolver resolver;
+  
+  // Build an interpreter to run the model
+  static tflite::MicroInterpreter static_interpreter(
+    model, resolver, tensor_arena, kTensorArenaSize);
+  interpreter = &static_interpreter;
+
+  // Allocate memory from the tensor_arena for the model's tensors
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    MicroPrintf("AllocateTensors() failed");
+    return;
+  }
+
+  // Assign model input and output buffers (tensors) to pointers
+  model_input = interpreter->input(0);
+  model_output = interpreter->output(0);
+
+  // Get information about the memory area to use for the model's input
+  // Supported data types:
+  // https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/c/common.h#L226
+#if DEBUG
+  Serial.print("Number of dimensions: ");
+  Serial.println(model_input->dims->size);
+  Serial.print("Dim 1 size: ");
+  Serial.println(model_input->dims->data[0]);
+  Serial.print("Dim 2 size: ");
+  Serial.println(model_input->dims->data[1]);
+  Serial.print("Input type: ");
+  Serial.println(model_input->type);
+#endif
+}
+
+void loop() {
+#if DEBUG
+  unsigned long start_timestamp = millis();
+#endif
+
+  // Get current timestamp and modulo with period
+  unsigned long timestamp = millis();
+  timestamp = timestamp % (unsigned long)period;
+
+  // Calculate x value to feed to the model
+  float x_val = ((float)timestamp * 2 * pi) / period;
+
+  // Copy value to input buffer (tensor)
+  model_input->data.f[0] = x_val;
+
+  // Run inference
+  TfLiteStatus invoke_status = interpreter->Invoke();
+  if (invoke_status != kTfLiteOk) {
+    MicroPrintf("Invoke failed on input: %f\n", x_val);
+    return;
+  }
+
+  // Read predicted y value from output buffer (tensor)
+  float y_val = model_output->data.f[0];
+
+  // Translate to a PWM LED brightness
+  int brightness = (int)(255 * y_val);
+  analogWrite(led_pin, brightness);
+
+  // Print value
+  Serial.println(y_val);
+
+#if DEBUG
+//  Serial.print("Time for inference (us): ");
+//  Serial.println(micros() - start_timestamp);
+#endif
+
+}
